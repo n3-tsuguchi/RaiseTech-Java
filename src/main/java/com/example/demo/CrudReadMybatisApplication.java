0a1a95e24366a576f92f22cadea9114129f65075
tsuguchi <tsuguchi@nnn.or.jp>
2025-09-28 01:28:20 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/CrudReadMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReadMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudReadMybatisApplication.class, args);
+	}
+
+}
